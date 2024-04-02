3a658ac468def503411ebdb3e9dc4a9112da278c
Luiz Sanches <luizsdev@gmail.com>
2024-04-02 20:41:44 -0300

    feat: render shirts in page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,35 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { ShirtCard } from "./components/ShirtCard";
+import { Shirt, fetchShirts } from "./mock/shirts";
+
 export default function Home() {
+  const [shirts, setShirts] = useState<Shirt[]>();
+  async function getShirts() {
+    const response = await fetchShirts();
+    setShirts(response);
+  }
+  useEffect(() => {
+    getShirts();
+  }, []);
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      teste
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 gap-6">
+      <h1 className="shadow flex rounded-lg bg-slate-200 w-3/4 p-6"></h1>
+      <section className="grid grid-cols-4 gap-6">
+        {shirts?.map((shirt) => {
+          return (
+            <ShirtCard
+              key={shirt.id}
+              id={shirt.id}
+              color={shirt.color}
+              name={shirt.name}
+              price={shirt.price}
+              size={shirt.size}
+            />
+          );
+        })}
+      </section>
     </main>
   );
 }
