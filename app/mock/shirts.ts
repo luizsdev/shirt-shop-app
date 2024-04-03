02cc8621fe0539629fc3b3dcb8ecd9b8040d082f
Luiz Sanches <luizsdev@gmail.com>
2024-04-02 22:26:59 -0300

    fix: correct mock images

--- app/mock/shirts.ts
+++ app/mock/shirts.ts
@@ -12,63 +12,58 @@ enum Size {
   G,
 }
 
-type Shirt = {
+export type Shirt = {
   id: number;
   name: string;
   color: Color;
   size: Size;
+  image: string;
   price: number;
 };
 
-const fetchShirts = (): Promise<Shirt[]> => {
+export const fetchShirts = (): Promise<Shirt[]> => {
   const shirtsData: Shirt[] = [
     {
       id: 1,
-      name: "Shirt 1",
+      name: "Camisa 1",
       color: Color.Red,
       size: Size.M,
       price: 100,
+      image:
+        "https://www.jplayinformatica.com.br/1369-large_default/camisa-vermelha-para-sublimacao-tamanho-m100-poliester.jpg",
     },
     {
       id: 2,
-      name: "Shirt 2",
+      name: "Camisa 2",
       color: Color.Blue,
       size: Size.G,
       price: 150,
-    },
-    {
-      id: 3,
-      name: "Shirt 3",
-      color: Color.Green,
-      size: Size.P,
-      price: 120,
-    },
-    {
-      id: 4,
-      name: "Shirt 4",
-      color: Color.Yellow,
-      size: Size.M,
-      price: 100,
-    },
-    {
-      id: 5,
-      name: "Shirt 5",
-      color: Color.Black,
-      size: Size.G,
-      price: 250,
+      image:
+        "https://images.tcdn.com.br/img/img_prod/809258/camiseta_azul_marinho_malha_pv_manga_curta_gola_redonda_231_1_6a96361a605204c923f396ff0a3d3e63.jpg",
     },
     {
       id: 6,
-      name: "Shirt 6",
+      name: "Camisa 6",
       color: Color.White,
       size: Size.P,
       price: 200,
+      image:
+        "https://dw0jruhdg6fis.cloudfront.net/producao/24866653/G/camisa_branca.jpg",
+    },
+    {
+      id: 5,
+      name: "Camisa 5",
+      color: Color.Black,
+      size: Size.G,
+      price: 250,
+      image:
+        "https://images.tcdn.com.br/img/img_prod/275189/camisa_preta_100_poliester_para_sublimacao_g_2697_1_20200722153318.jpg",
     },
   ];
 
   return new Promise((resolve) => {
     setTimeout(() => {
       resolve(shirtsData);
-    }, 2000);
+    }, 300);
   });
 };
