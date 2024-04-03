6751c9e7ff2daabdd2487b96b46dd522b903adad
Luiz Sanches <luizsdev@gmail.com>
2024-04-02 22:26:51 -0300

    chore: add deps and config

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["dw0jruhdg6fis.cloudfront.net"],
+    domains: [
+      "dw0jruhdg6fis.cloudfront.net",
+      "www.jplayinformatica.com.br",
+      "images.tcdn.com.br",
+    ],
   },
 };
 
